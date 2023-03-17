e5251a363b54e19b22ffe8b8caec2663e64bb177
swsong <ssw890720@gmail.com>
2023-03-17 19:25:13 +0900

    다른 클래스의 필드를 참조한다면 함수의 의동을 고려해라

--- src/main/java/me/whiteship/refactoring/_07_divergent_change/_25_move_function/AccountType.java
+++ src/main/java/me/whiteship/refactoring/_07_divergent_change/_25_move_function/AccountType.java
@@ -10,4 +10,19 @@ public class AccountType {
     public boolean isPremium() {
         return this.premium;
     }
+
+    // 다른클래스의 필드를 많이 참조하고 있다면 이동을 고려한다.
+    // 순환참조가 발생하고 하나의 필드만 사용하기 때문에 객체를 주입받지 않는다.
+    double overdraftCharge(int daysOverdrawn) {
+        if (this.premium) {
+            final int baseCharge = 10;
+            if (daysOverdrawn <= 7) {
+                return baseCharge;
+            } else {
+                return baseCharge + (daysOverdrawn - 7) * 0.85;
+            }
+        } else {
+            return daysOverdrawn * 1.75;
+        }
+    }
 }
