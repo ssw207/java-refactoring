45f936be466d6a50d375bd46a534c3a46272d1ab
swsong <ssw890720@gmail.com>
2023-03-17 19:20:34 +0900

    비즈니스가 단계별로 진행된다면 단계별로 메서드를 분리한다. 중간단계를 두고 매개변수를 줄이기위해 중간단계 클래스를 추가한다

--- src/main/java/me/whiteship/refactoring/_07_divergent_change/_24_split_phase/PriceOrder.java
+++ src/main/java/me/whiteship/refactoring/_07_divergent_change/_24_split_phase/PriceOrder.java
@@ -3,13 +3,22 @@ package me.whiteship.refactoring._07_divergent_change._24_split_phase;
 public class PriceOrder {
 
     public double priceOrder(Product product, int quantity, ShippingMethod shippingMethod) {
+        final PriceData priceData = calculatePriceData(product, quantity); // 가격데이터를 생성
+        return applyShipping(priceData, shippingMethod); // 생성한 가격에 배송비를 적용
+    }
+
+    private PriceData calculatePriceData(Product product, int quantity) {
         final double basePrice = product.basePrice() * quantity;
         final double discount = Math.max(quantity - product.discountThreshold(), 0)
                 * product.basePrice() * product.discountRate();
-        final double shippingPerCase = (basePrice > shippingMethod.discountThreshold()) ?
+
+        return new PriceData(basePrice, discount, quantity); // 가격과 연관된 변수를 클래스에 담는다
+    }
+
+    private double applyShipping(PriceData priceData, ShippingMethod shippingMethod) {
+        final double shippingPerCase = (priceData.basePrice() > shippingMethod.discountThreshold()) ?
                 shippingMethod.discountedFee() : shippingMethod.feePerCase();
-        final double shippingCost = quantity * shippingPerCase;
-        final double price = basePrice - discount + shippingCost;
-        return price;
+        final double shippingCost = priceData.quantity() * shippingPerCase;
+        return priceData.basePrice() - priceData.discount() + shippingCost;
     }
 }
