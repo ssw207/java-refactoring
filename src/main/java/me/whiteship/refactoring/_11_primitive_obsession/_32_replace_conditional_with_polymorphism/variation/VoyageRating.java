31ff1c70b51e5af1a8701d0dd007f6193ba8c74e
swsong <ssw890720@gmail.com>
2023-03-19 12:02:25 +0900

    특정조건에서만 다른동작을 한다면 서브클래스로 분리한다

--- src/main/java/me/whiteship/refactoring/_11_primitive_obsession/_32_replace_conditional_with_polymorphism/variation/VoyageRating.java
+++ src/main/java/me/whiteship/refactoring/_11_primitive_obsession/_32_replace_conditional_with_polymorphism/variation/VoyageRating.java
@@ -2,11 +2,15 @@ package me.whiteship.refactoring._11_primitive_obsession._32_replace_conditional
 
 import java.util.List;
 
+/**
+ * Voyage zone 값에 따라 다른 동작을 함
+ * Voyage zone 을 클래스로 분리해 관련로직을 이동한다.
+ */
 public class VoyageRating {
 
-    private Voyage voyage;
+    protected Voyage voyage;
 
-    private List<VoyageHistory> history;
+    protected List<VoyageHistory> history;
 
     public VoyageRating(Voyage voyage, List<VoyageHistory> history) {
         this.voyage = voyage;
@@ -20,11 +24,14 @@ public class VoyageRating {
         return (vpf * 3 > (vr + chr * 2)) ? 'A' : 'B';
     }
 
-    private int captainHistoryRisk() {
+    protected int captainHistoryRisk() {
         int result = 1;
         if (this.history.size() < 5) result += 4;
         result += this.history.stream().filter(v -> v.profit() < 0).count();
-        if (this.voyage.zone().equals("china") && this.hasChinaHistory()) result -= 2;
+        return getPositive(result);
+    }
+
+    protected int getPositive(int result) {
         return Math.max(result, 0);
     }
 
@@ -33,30 +40,25 @@ public class VoyageRating {
         if (this.voyage.length() > 4) result += 2;
         if (this.voyage.length() > 8) result += this.voyage.length() - 8;
         if (List.of("china", "east-indies").contains(this.voyage.zone())) result += 4;
-        return Math.max(result, 0);
+        return getPositive(result);
     }
 
-    private int voyageProfitFactor() {
+    protected int voyageProfitFactor() {
         int result = 2;
 
         if (this.voyage.zone().equals("china")) result += 1;
         if (this.voyage.zone().equals("east-indies")) result +=1 ;
-        if (this.voyage.zone().equals("china") && this.hasChinaHistory()) {
-            result += 3;
-            if (this.history.size() > 10) result += 1;
-            if (this.voyage.length() > 12) result += 1;
-            if (this.voyage.length() > 18) result -= 1;
-        } else {
-            if (this.history.size() > 8) result +=1 ;
-            if (this.voyage.length() > 14) result -= 1;
-        }
+        result += voyageLengthFactor();
+        result += historyLengthFactor();
 
         return result;
     }
 
-    private boolean hasChinaHistory() {
-        return this.history.stream().anyMatch(v -> v.zone().equals("china"));
+    protected int voyageLengthFactor() {
+        return (this.voyage.length() > 14) ? -1 : 0;
     }
 
-
+    protected int historyLengthFactor() {
+        return (this.history.size() > 8) ? 1 : 0 ;
+    }
 }
